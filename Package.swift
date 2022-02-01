341fcd231c0c5a8dda2dcb0010367d74cdf1dcca
Sathyan Elangovan <sathya576@gmail.com>
2022-02-01 11:30:01 +0530

    changed the path

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .target(
             name: "KommunicateCore-iOS-SDK",
             dependencies: [],
-            path: "Sources",
+            path: "Sources/KommunicateCore-iOS-SDK",
             exclude: ["Info.plist",
                       "MQTT/MQTTClient-Prefix.pch"],
             resources: [
